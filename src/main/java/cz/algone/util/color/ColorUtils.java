be07db5bf52f8f46360fadbfbc6f3d71d557fd3f
Gabr1iel <gabr1i3l.v@gmail.com>
2025-12-22 22:51:52 +0100

    Refactor ColorUtils -vytvoření Rgb recordu pro ukládání jednotlivých int hodnot -vytvoření metody darken pro patterny -metody interpolateColors a darken pracují s Rgb místo samostatného výpočtu

--- src/main/java/cz/algone/util/color/ColorUtils.java
+++ src/main/java/cz/algone/util/color/ColorUtils.java
@@ -4,27 +4,40 @@ import javafx.scene.paint.Color;
 
 public class ColorUtils {
     public static final ColorPair DEFAULT_COLORPICKER_COLOR = new ColorPair(Color.BLACK, null);
+    public static final double DEFAULT_CHECKERPATTERN_DARKEN = 0.25;
 
     public static int interpolateColor(Color primary, Color secondary, float t) {
-        int r1 = (int) Math.round(primary.getRed() * 255);
-        int g1 = (int) Math.round(primary.getGreen() * 255);
-        int b1 = (int) Math.round(primary.getBlue() * 255);
-
+        Rgb rgb1 = parseColorToInt(primary);
         int r, g, b;
 
         if (!(secondary == null)) {
-            int r2 = (int) Math.round(secondary.getRed() * 255);
-            int g2 = (int) Math.round(secondary.getGreen() * 255);
-            int b2 = (int) Math.round(secondary.getBlue() * 255);
-
-            r = (int) (r1 + (r2 - r1) * t);
-            g = (int) (g1 + (g2 - g1) * t);
-            b = (int) (b1 + (b2 - b1) * t);
+            Rgb rgb2 = parseColorToInt(secondary);
+            r = (int) (rgb1.r() + (rgb2.r() - rgb1.r()) * t);
+            g = (int) (rgb1.g() + (rgb2.g() - rgb1.g()) * t);
+            b = (int) (rgb1.b() + (rgb2.b() - rgb1.b()) * t);
         } else {
-            r = r1;
-            g = g1;
-            b = b1;
+            r = rgb1.r();
+            g = rgb1.g();
+            b = rgb1.b();
         }
         return (0xFF << 24) | (r << 16) | (g << 8) | b;
+    }
+
+    public static int darken(Color color, double amount) {
+        amount = Math.max(0, Math.min(1, amount));
+        Rgb rgb1 = parseColorToInt(color);
+
+        int r = (int) Math.round(rgb1.r() * (1.0 - amount));
+        int g = (int) Math.round(rgb1.g() * (1.0 - amount));
+        int b = (int) Math.round(rgb1.b() * (1.0 - amount));
+
+        return (0xFF << 24) | (r << 16) | (g << 8) | b;
+    }
+
+    public static Rgb parseColorToInt(Color color) {
+        int r = (int) Math.round(color.getRed() * 255);
+        int g = (int) Math.round(color.getGreen() * 255);
+        int b = (int) Math.round(color.getBlue() * 255);
+        return new Rgb(r, g, b);
     }
 }
---
+++ src/main/java/cz/algone/util/color/Rgb.java
@@ -0,0 +1,3 @@
+package cz.algone.util.color;
+
+public record Rgb(int r, int g, int b) {}
